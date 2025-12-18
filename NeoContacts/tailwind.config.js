dface583f4628a5b1b9a3c3f5b42e8a9fed600ff
rm1710 <riteshm475@gmail.com.com>
2025-12-18 21:48:47 +0530

    dark light theme started

--- NeoContacts/tailwind.config.js
+++ NeoContacts/tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: ["./src/main/resources/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
   darkMode:"selector",
-}
+};
 
