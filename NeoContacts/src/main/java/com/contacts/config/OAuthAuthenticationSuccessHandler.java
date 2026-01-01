6d726ba6d1294b062573658d4953424e8c5a42bc
rm1710 <riteshm475@gmail.com.com>
2026-01-01 22:33:32 +0530

    authentication and login part completed

--- NeoContacts/src/main/java/com/contacts/config/OAuthAuthenticationSuccessHandler.java
+++ NeoContacts/src/main/java/com/contacts/config/OAuthAuthenticationSuccessHandler.java
@@ -23,77 +23,120 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-
 @Component
 public class OAuthAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 
     Logger logger = LoggerFactory.getLogger(OAuthAuthenticationSuccessHandler.class);
-    
+
     @Autowired
     private UserRepo userRepo;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
             Authentication authentication) throws IOException, ServletException {
-                logger.info("OAuthAuthenticationSuccessHandler");
+         logger.info("OAuthAuthenicationSuccessHandler");
 
-                var oauth2Authentication = (OAuth2AuthenticationToken) authentication.getPrincipal();
+        // identify the provider
 
-                String authorizedClientRegistrationId = oauth2Authentication.getAuthorizedClientRegistrationId();
+        var oauth2Authenication = (OAuth2AuthenticationToken) authentication;
 
-                logger.info(authorizedClientRegistrationId);
+        String authorizedClientRegistrationId = oauth2Authenication.getAuthorizedClientRegistrationId();
 
-                logger.info(authorizedClientRegistrationId);
-                if(authorizedClientRegistrationId.equalsIgnoreCase("google")){
-                    // google attributes
-                }else if(authorizedClientRegistrationId.equalsIgnoreCase("github")){
-                    // github attributes
-                }
-                else if(authorizedClientRegistrationId.equalsIgnoreCase("linkedin")){
-                    // linkedin attributes
-                }else{
-                    logger.info("login with unsupported provider");
-                }
-                
-                // DefaultOAuth2User user=(DefaultOAuth2User) authentication.getPrincipal();
+        logger.info(authorizedClientRegistrationId);
 
-                // // logger.info(user.getName());
-                // // user.getAttributes().forEach((key,value)->{
-                // //     logger.info("{}=>{}",key,value);
-                // // });
+        var oauthUser = (DefaultOAuth2User) authentication.getPrincipal();
 
-                // // logger.info(user.getAuthorities().toString());
+        oauthUser.getAttributes().forEach((key, value) -> {
+            logger.info(key + " : " + value);
+        });
 
-                // String email = user.getAttribute("email").toString();
-                // String name = user.getAttribute("name").toString();
-                // String picture=user.getAttribute("picture").toString();
+        User user = new User();
+        user.setUserId(UUID.randomUUID().toString());
+        user.setRoleList(List.of(AppConstants.ROLE_USER));
+        user.setEmailVerified(true);
+        user.setEnabled(true);
 
+        logger.info(authorizedClientRegistrationId);
+        if (authorizedClientRegistrationId.equalsIgnoreCase("google")) {
 
-                // User user1= new User();
-                // user1.setEmail(email);
-                // user1.setName(name);
-                // user1.setProfilePic(picture);
-                // user1.setPassword("password");
-                // user1.setUserId(UUID.randomUUID().toString());
-                // user1.setProvider(Providers.GOOGLE);
-                // user1.setEnabled(true);
+            // google
+            // google attributes
 
+            user.setEmail(oauthUser.getAttribute("email").toString());
+            user.setProfilePic(oauthUser.getAttribute("picture").toString());
+            user.setName(oauthUser.getAttribute("name").toString());
+            user.setProviderUserId(oauthUser.getName());
+            user.setProvider(Providers.GOOGLE);
+            user.setAbout("This account is created using google.");
 
-                // user1.setEmailVerified(true);
-                // user1.setProviderUserId(user.getName());
-                // user1.setRoleList(List.of(AppConstants.ROLE_USER));
-                // user1.setAbout("this account is created using google oauth2 login");
-                
-                // User user2=userRepo.findByEmail(email).orElse(null);
-                // if(user2==null){
-                //     userRepo.save(user1);
-                //     logger.info("new user created");
-                // }
+        } else if (authorizedClientRegistrationId.equalsIgnoreCase("github")) {
 
-                 
+            // github
+            // github attributes
+            String email = oauthUser.getAttribute("email") != null ? oauthUser.getAttribute("email").toString()
+                    : oauthUser.getAttribute("login").toString() + "@gmail.com";
+            String picture = oauthUser.getAttribute("avatar_url").toString();
+            String name = oauthUser.getAttribute("login").toString();
+            String providerUserId = oauthUser.getName();
 
-                new DefaultRedirectStrategy().sendRedirect(request, response, "/user/profile");
-        
-            }
+            user.setEmail(email);
+            user.setProfilePic(picture);
+            user.setName(name);
+            user.setProviderUserId(providerUserId);
+            user.setProvider(Providers.GITHUB);
+
+            user.setAbout("This account is created using github");
+        }
+
+        else if (authorizedClientRegistrationId.equalsIgnoreCase("linkedin")) {
+
+        }
+
+        else {
+            logger.info("OAuthAuthenicationSuccessHandler: Unknown provider");
+        }
+
+        // DefaultOAuth2User user=(DefaultOAuth2User) authentication.getPrincipal();
+
+        // logger.info(user.getName());
+        // user.getAttributes().forEach((key,value)->{
+        // logger.info("{}=>{}",key,value);
+        // });
+
+        // logger.info(user.getAuthorities().toString());
+
+        // String email = user.getAttribute("email").toString();
+        // String name = user.getAttribute("name").toString();
+        // String picture=user.getAttribute("picture").toString();
+
+        // User user1= new User();
+        // user1.setEmail(email);
+        // user1.setName(name);
+        // user1.setProfilePic(picture);
+        // user1.setPassword("password");
+        // user1.setUserId(UUID.randomUUID().toString());
+        // user1.setProvider(Providers.GOOGLE);
+        // user1.setEnabled(true);
+
+        // user1.setEmailVerified(true);
+        // user1.setProviderUserId(user.getName());
+        // user1.setRoleList(List.of(AppConstants.ROLE_USER));
+        // user1.setAbout("this account is created using google oauth2 login");
+
+        // User user2=userRepo.findByEmail(email).orElse(null);
+        // if(user2==null){
+        // userRepo.save(user1);
+        // logger.info("new user created");
+        // }
+
+        User user2 = userRepo.findByEmail(user.getEmail()).orElse(null);
+        if(user2==null){
+            userRepo.save(user);
+            logger.info("new user created");
+        }
+
+        new DefaultRedirectStrategy().sendRedirect(request, response, "/user/profile");
+
+    }
 
 }
--- NeoContacts/src/main/java/com/contacts/controllers/UserController.java
+++ NeoContacts/src/main/java/com/contacts/controllers/UserController.java
@@ -1,13 +1,24 @@
 package com.contacts.controllers;
 
+import java.security.Principal;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.contacts.exceptions.Helper;
 
 
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
 
     //user dashboard
     @RequestMapping(value = "/dashboard")
@@ -17,7 +28,11 @@ public class UserController {
     }
 
     @RequestMapping(value="/profile")
-    public String userProfile() {
+    public String userProfile(Authentication authentication) {
+        String username = Helper.getEmailOfLoggedInUser(authentication);
+
+        logger.info("User Logged: {}"+username);
+
         return "user/profile";
     }
     
